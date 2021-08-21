da202490a6c27426db446d17b4d95004cdc45073
Yare15 <86269883+Yare15@users.noreply.github.com>
2021-08-20 19:13:13 -0500

    Add files via upload

---
+++ himno.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    printf("alcanzamos por fin la victoria\n");
+    printf("En el campo feliz de la union;\n");
+    printf("Con ardientes fulgores de gloria\n");
+    printf("Se ilumina la nueva nacion\n");
+    printf("\n");
+    printf("ESTROFAS\n");
+    printf("\n");
+    printf("Es preciso cubrir con un velo\n");
+    printf("Del pasado el calvario y la cruz;\n");
+    printf("Y que adorne el azul de tu cielo\n");
+    printf("De concordia la esplendida luz\n");
+    printf("\n");
+    printf("El progreso acaricia tus lares.\n");
+    printf("Al compas de sublime cancion\n");
+    printf("Ves rugir a tus pies ambos mares\n");
+    printf("Que dan rumbo a tu noble mision.\n");
+    printf("\n");
+    printf("(Coro)\n");
+    printf("\n");
+    printf("En tu suelo cubierto de flores\n");
+    printf("A los besos del tibio terral,\n");
+    printf("Terminaron guerreros fragores;\n");
+    printf("Solo reina el amor fraternal.\n");
+    printf("\n");
+    printf("Adelante la pica y la pala,\n");
+    printf("Al trabajo sin mas dilacion,\n");
+    printf("Y seremos asi prez y gala\n");
+    printf("De este mundo feraz de Clolon.\n");
+    return 0;
+}
