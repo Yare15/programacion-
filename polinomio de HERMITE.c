bdbaa5964c96a9ea020aeaf41408d4d87dbdff14
Yare15 <86269883+Yare15@users.noreply.github.com>
2021-08-28 19:39:50 -0500

    Add files via upload

---
+++ polinomio de HERMITE.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+float hermiteRec(int n, float num)
+{
+	if(n==0){
+		return 1;
+	}
+	else if (n==1)
+	{
+		return 2*num;
+	}
+	else
+	{
+		return 2*num*hermiteRec((n-1),num) - 2*(n-1)*hermiteRec((n-2),num); 
+	}
+	
+}
+
+int main()
+{
+	int n;
+	float x;
+	printf ("\t\t\tPOLINOMIOS DE HERMITE\n\n");	    
+	printf("Escriba un grado de polinomio: ");
+	scanf("%d", & n);
+	printf("Escriba el numero real: ");
+	scanf("%f", & x);
+	printf("\n\n El resultado es: %.2f", hermiteRec(n,x));
+}
